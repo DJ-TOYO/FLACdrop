4ddf908d336e9b8be9e0cca9e10ad95866f4a10b
DJ TOYO <toyosl1200mk5@gmail.com>
2026-09-19 09:53:47 +0900

    コメント文字化け修正

--- FLACdrop/encoders.h
+++ FLACdrop/encoders.h
@@ -81,8 +81,8 @@ typedef enum
 #define WAVE_FORMAT_UNKNOWN		0x0000	// Microsoft unknown format
 #define WAVE_FORMAT_PCM			0x0001	// PCM
 #define WAVE_FORMAT_IEEE_FLOAT	0x0003	// IEEE float
-#define WAVE_FORMAT_ALAW		0x0006	// 8 - bit ITU - T G.711 A - law
-#define WAVE_FORMAT_MULAW		0x0007	// 8 - bit ITU - T G.711 µ - law
+#define WAVE_FORMAT_ALAW        0x0006  // 8-bit ITU-T G.711 A-law
+#define WAVE_FORMAT_MULAW       0x0007  // 8-bit ITU-T G.711 É -law
 #define WAVE_FORMAT_EXTENSIBLE	0xFFFE	// Determined by SubFormat
 
 // libmp3lame encoding bitrates
